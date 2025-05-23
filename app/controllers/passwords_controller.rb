5ef9a3a07992309d431ad528281d5f84eeed09bb
inaidE <iperminov09@gmail.com>
2025-05-23 21:02:56 +0300

    Do a small change in password_controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,8 +11,16 @@ class PasswordsController < ApplicationController
       return render "pages/profile"
     end
 
+    if params[:current_password] == params[:new_password]
+      flash.now[:alert] = "Новый пароль должен отличатсья от старого"
+      return render "pages/profile"
+    end
     if current_user.update(password: params[:new_password],
                            password_confirmation: params[:new_password_confirmation])
+      AuditLog.create!(
+        user:         current_user,
+        event_type:   :data_change
+      )
       redirect_to profile_page_path, notice: "Пароль успешно изменён"
     else
       flash.now[:alert] = "Пароли не совпадают"
